31b85b6642240f6af34f0b0c115404b7e3105508
Victoria <victoriashchekaturova@gmail.com>
2026-04-02 10:13:36 +0600

    second commit

--- index.ts
+++ index.ts
@@ -15,3 +15,36 @@ console.log(toyota);
 
 const bmw = new Car("BMW", "X5", 2020);
 console.log(bmw);
+// -------------------------------------------------------
+
+class Student {
+  public name: string;
+  private course: number;
+
+  constructor(name: string, course: number) {
+    this.name = name;
+    this.course = course;
+  }
+
+  levelUp() {
+    if (this.course < 5) {
+      this.course++;
+    } else {
+      console.log("Студент уже на последнем курсе!");
+    }
+  }
+
+  getCourse() {
+    return this.course;
+  }
+}
+
+const student = new Student("Max", 1);
+console.log(student.getCourse()); //1
+student.levelUp(); //2
+student.levelUp(); //3
+student.levelUp(); //4
+student.levelUp(); //5
+student.levelUp(); //Студент уже на последнем курсе!
+
+console.log(student.getCourse()); //5
